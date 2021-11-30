d5ea77e96de6f951cbfd69640f4d536bfd3daf22
unknown <sum37@kaist.ac.kr>
2021-11-30 17:49:32 +0900

    loginconsol basic

---
+++ src/component/header.js
@@ -0,0 +1,9 @@
+export default function Header(){
+    return (
+        <div>
+            <h1>Personal Diary</h1>
+            <p>Please log in and go to your page</p>
+        </div>
+    
+    );
+}
---
+++ src/component/login.js
@@ -0,0 +1,12 @@
+export default function Login(){
+    return (
+        <div className="loginconsol">
+            <h1>Type your id and password</h1>
+            <div className="button">
+                <a href="#x" className="link">
+                    뭘해야하지?
+                </a>
+            </div>
+        </div>
+    );
+}
