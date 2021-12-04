ca906acc506e3b0c0bb8799500f4a11ea030245a
unknown <sum37@kaist.ac.kr>
2021-12-04 13:41:36 +0900

    Make db.js

---
+++ backend/src/db.js
@@ -0,0 +1,24 @@
+const TodoModel = require("./models/todo")
+function add(name, callback) {
+  const newItem = new ListModel({
+    name
+  });
+  newItem.save((error, result) => {
+    callback(result);
+  })
+}
+function getAll(callback){
+  ListModel.find({}, (error, result)=>{
+    if(error){
+      console.log(error);
+      callback([]);
+    }
+    else {
+      callback(result);
+    }
+  })
+}
+module.exports = {
+  getAll,
+  add,
+};
