d6be55aba561d5e9f295c0958a6957af878e3eac
unknown <sum37@kaist.ac.kr>
2021-12-05 03:06:10 +0900

    Selectmotion.js is combined into write.js.

--- src/pages/write.js
+++ src/pages/write.js
@@ -1,8 +1,12 @@
 import React, { useEffect, useState } from "react";
 import axios from 'axios';
-import ListItem from "../component/ListItem";
-import SelectEmotion from "../component/selectemotion";
+import InputItem from "../component/InputItem";
 import './write.css';
+
+const happys = ["0", "1", "2", "3", "4", "5"];
+const angrys = ["0", "1", "2", "3", "4", "5"];
+const sads = ["0", "1", "2", "3", "4", "5"];
+const joys = ["0", "1", "2", "3", "4", "5"];
 
 export default function WritePage(){
     const diarysectionstyle={
@@ -11,9 +15,13 @@ export default function WritePage(){
         maxlength: '20'
     };
 
-    
     const [input, setInput] = useState([]);
     const [saveInput, setSaveInput] = useState("");
+
+    const [happy, setHappy] = useState("0");
+    const [angry, setAngry] = useState("0");
+    const [sad, setSad] = useState("0");
+    const [joy, setJoy] = useState("0");
     
     useEffect(() => {
         // 목록 조회 요청 전송
@@ -25,10 +33,10 @@ export default function WritePage(){
       }, []);
     
     const onSaveClick = () => {
-        axios.post('api/write', {
+        axios.post('/api/write', {
             input: saveInput
         })
-        .then(() => axios.get('api/write'))
+        .then(() => axios.get('/api/write'))
         .then(response => {
             setInput(response.data);
             setSaveInput("");
@@ -36,19 +44,89 @@ export default function WritePage(){
         alert('저장하시겠습니까?');
     };
 
-    const ListUp = input.map(v=>(
-        <ListItem
-            input={v.input}
+    const ListUp = input.map( (v,i)=>(
+        <InputItem
+            key={v._id}
+            happy={v.happyvalue}
+            angry={v.angryvalue}
+            sad={v.sadvalue}
+            joy={v.joyvalue}
+            title={v.titlevalue}
+            body={v.bodyvalue}
         />
     ));
 
     return (
         <div>
-            <header className="intro">
+            <div className="intro">
                 <h1>오늘 하루는 어땠나요?</h1>
                 <h2>각각의 감정을 느낀 정도를 숫자를 통해 표현해보세요.</h2>
-            </header>
-            <SelectEmotion />
+            </div>
+            <div className="chooseemotion">
+                <div className="happy">
+                    <label> 희 </label>
+                    {happys.map(f => (
+                    <>
+                        <input
+                            type="radio"
+                            name="happy"
+                            value={f}
+                            checked={happy===f}
+                            onChange={e=>setHappy(e.currentTarget.value)}
+                        />{" "}
+                        {f}
+                    </>    
+                    ))}
+                </div>
+                <br />
+                <div className="angry">
+                    <label> 노 </label>
+                    {angrys.map(f => (
+                    <>
+                        <input
+                            type="radio"
+                            name="angry"
+                            value={f}
+                            checked={angry===f}
+                            onChange={e=>setAngry(e.currentTarget.value)}
+                        />{" "}
+                        {f}
+                    </>    
+                    ))}
+                </div>
+                <br />
+                <div className="sad">
+                    <label> 애 </label>
+                    {sads.map(f => (
+                    <>
+                        <input
+                            type="radio"
+                            name="sad"
+                            value={f}
+                            checked={sad===f}
+                            onChange={e=>setSad(e.currentTarget.value)}
+                        />{" "}
+                        {f}
+                    </>    
+                    ))}
+                </div>
+                <br />
+                <div className="joy">
+                    <label> 락 </label>
+                    {joys.map(f => (
+                    <>
+                        <input
+                            type="radio"
+                            name="joy"
+                            value={f}
+                            checked={joy===f}
+                            onChange={e=>setJoy(e.currentTarget.value)}
+                        />{" "}
+                        {f}
+                    </>    
+                    ))}
+                </div>
+        </div>
                 <br />
                 <textarea
                     className="textarea"
@@ -58,6 +136,7 @@ export default function WritePage(){
                     required/>
                 <br />
                 <button onClick={()=>onSaveClick()}>저장</button>
+                {ListUp}
         </div>
     );
 }
